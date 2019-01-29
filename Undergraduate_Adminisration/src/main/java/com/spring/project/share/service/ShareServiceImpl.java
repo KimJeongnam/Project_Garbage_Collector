cd2f0042e012294d59fa43a6a3ee91934ed95c0b
KimJeongnam <kim910712@gmail.com>
2019-01-29 19:46:54 +0900

    userImage modify

--- Undergraduate_Adminisration/src/main/java/com/spring/project/share/service/ShareServiceImpl.java
+++ Undergraduate_Adminisration/src/main/java/com/spring/project/share/service/ShareServiceImpl.java
@@ -4,6 +4,8 @@ import java.util.Collection;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -11,24 +13,39 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.spring.project.share.dao.ShareDAO;
+import com.spring.project.share.vo.ShareUserInfo;
+
 @Service
 public class ShareServiceImpl implements ShareService{
+	@Autowired
+	ShareDAO dao;
 
 	@Override
-	public String loginSucces(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+	public String loginSucces(HttpServletRequest request, RedirectAttributes redirectAttributes, Logger logger) {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		Collection<? extends GrantedAuthority>  authorities = ((User) authentication.getPrincipal()).getAuthorities();
 		String redirectUrl ="";
 		
-		if(authorities.stream().filter(o->o.getAuthority().equals("ROLE_ADMIN")).findAny().isPresent())
-			redirectUrl = "/admin/index";
-		else if(authorities.stream().filter(o->o.getAuthority().equals("ROLE_PROFESSOR")).findAny().isPresent())
-			redirectUrl = "/professor/index";
-		else if(authorities.stream().filter(o->o.getAuthority().equals("ROLE_STUDENT")).findAny().isPresent())
-			redirectUrl = "/student/index";
+		ShareUserInfo user = null;
+		String userNumber = (String)request.getSession().getAttribute("userNumber");
 		
-		String userId = (String)request.getSession().getAttribute("userId");
-		redirectAttributes.addFlashAttribute("message","환영합니다. '"+userId+"' 님");
+		
+		if(authorities.stream().filter(o->o.getAuthority().equals("ROLE_ADMIN")).findAny().isPresent()) {
+			user = dao.getEmployeeInfo(userNumber);
+			redirectUrl = "/admin/index";
+		}else if(authorities.stream().filter(o->o.getAuthority().equals("ROLE_PROFESSOR")).findAny().isPresent()) {
+			user = dao.getEmployeeInfo(userNumber);
+			redirectUrl = "/professor/index";
+		}else if(authorities.stream().filter(o->o.getAuthority().equals("ROLE_STUDENT")).findAny().isPresent()) {
+			redirectUrl = "/student/index";
+			user = dao.getStudentInfo(userNumber);
+		}
+		
+		
+		logger.info(user.toString());
+		request.getSession().setAttribute("user", user);
+		redirectAttributes.addFlashAttribute("message","환영합니다. '"+user.getUserName()+"' 님");
 		return redirectUrl;
 	}
 	
